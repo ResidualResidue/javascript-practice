05006f50c17d86258f769c39bd55fbf8fdef4e1c
ResidualResidue <michaelfoley1999@gmail.com>
2024-08-17 23:58:00 -0400

    Some basic DOM manipulation

--- home.js
+++ home.js
@@ -1,2 +1,13 @@
 console.log("Hello, world!");
-alert(1);
+//alert(1);
+
+// variables
+
+var v = "smoothie";
+
+//console.log(a);
+console.log(v);
+
+
+var age = prompt("What is your age?");
+document.getElementById('header').innerHTML = age;
