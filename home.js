673519f8005e20a4b23cb97c84806e5f5094f1d1
Michael Foley <michaelfoley1999@gmail.com>
2024-08-22 23:47:37 -0400

    Played with some functions

--- home.js
+++ home.js
@@ -8,6 +8,24 @@ var v = "smoothie";
 //console.log(a);
 console.log(v);
 
+/* Functions
+1. Create
+2. Call
+*/
 
 var age = prompt("What is your age?");
 document.getElementById('header').innerHTML = age;
+function fun() {
+    console.log('this is a function');
+}
+
+/* 
+    say hello
+*/
+function sayHello(age){
+    var name = prompt("What is your name?")
+    alert("Hello, "+ name + "! It is shocking you are "+ age+" years old!");
+}
+
+fun();
+sayHello(age);
